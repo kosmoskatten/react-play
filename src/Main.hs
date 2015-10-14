99a11313158839293a6bd44423b81e3d8bc1f2f2
Patrik Sandahl <kosmoskatten.software@gmail.com>
2015-10-14 15:54:00 +0200

    Simple fetching of comments from server.

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,44 @@
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE TypeFamilies  #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
 module Main
     ( main
     ) where
 
+import Control.Monad.Trans.Either (EitherT)
+import Data.Aeson (ToJSON)
+import Data.Text (Text)
+import GHC.Generics (Generic)
 import Network.Wai (Application)
 import Network.Wai.Handler.Warp (run)
-import Servant (Proxy (..), Raw, Server, serve, serveDirectory)
+import Servant
 
-type API = Raw
+data Comment =
+    Comment { author :: !Text
+            , text   :: !Text
+            }
+    deriving (Generic, Show)
+
+instance ToJSON Comment
+
+type API = "api" :> "comments" :> Get '[JSON] [Comment] 
+      :<|> Raw
 
 api :: Proxy API
 api = Proxy
 
+comments :: EitherT ServantErr IO [Comment]
+comments = return [ Comment { author = "Sigge Pigg"
+                            , text = "A little meow" }
+                  , Comment { author = "Frasse Tass"
+                            , text = "Just *another* meow" }
+                  ]
+
 server :: Server API
-server = serveDirectory "static"
+server = comments
+    :<|> serveDirectory "static"
 
 app :: Application
 app = serve api server
